3bfb57330242f966c559128b0c99a470d8bff513
Nikos P <nikosp2196@gmail.com>
2019-12-29 02:44:23 +0200

    Prepared the first script for the first dataset.

--- sigma_1.c
+++ sigma_1.c
@@ -2,16 +2,96 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
+
+#define UPPER 2
+#define LOWER -2
+
+/**
+ * Returns a string with the category and the pair
+ * format: category x1 x2
+ **/
+char* getCategory(float x1, float x2);
+
+/**
+ * Generates a pair of random floats.
+ **/
+void generateRandomPair(float* x1, float* x2);
+
+int c1 = 0;
+int c2 = 0;
+int c3 = 0;
 
 int main(){
+    char buff[50];
+    float x,y;
     FILE* fptr;
-    char buf[50];
-    float number = 123.90342;
-    
-    gcvt(number,6, buf);
-    strcat(buf, "\n");
-    fptr = fopen("data_1.txt", "w");
-    fputs(buf, fptr);
-    fputs("Molly", fptr);
+    srand(time(0));
+
+    /* First create a train set and then a the test set.
+       For the train set just remove the noise if from the comments.
+    */
+    fptr = fopen("test.txt", "w");
+    for(int i = 0; i<3000; i++){
+        generateRandomPair(&x, &y);
+        strcpy(buff, getCategory(x, y));
+        fputs(buff, fptr);
+    }
+    printf("c1: %d\nc2: %d\nc3: %d\n", c1,c2,c3);
     fclose(fptr);
 }
+
+char* getCategory(float x1, float x2){
+    static char category[50];
+    char str1[15];
+    char str2[15];
+    
+    if ((powf(x1-1, 2) + powf(x2-1, 2)) <= 0.49){
+        strcpy(category, "C2");
+        c2++;
+    }else if ((powf(x1+1, 2) + powf(x2+1, 2)) <= 0.49){
+        strcpy(category, "C2");
+        c2++;
+    }else if ((powf(x1+1, 2) + powf(x2-1, 2)) <= 0.49){
+        strcpy(category, "C3");
+        c3++;
+    }else if((powf(x1-1, 2) + powf(x2+1, 2)) <= 0.49){
+        strcpy(category, "C3");
+        c3++;
+    }else{
+        strcpy(category, "C1");
+        c1++;
+    }
+
+    //NOISE IF
+    /*
+    if(!strcmp(category, "C1")){
+        float probability = rand() % 100;
+        if (probability<10){
+            //printf("%f\n", probability);
+            strcpy(category,"C1");
+        }
+    }
+    */
+
+
+    gcvt(x1, 8, str1);
+    gcvt(x2, 8, str2);
+
+    strcat(category, " ");
+    strcat(category, str1);
+    strcat(category, " ");
+    strcat(category, str2);
+    strcat(category, "\n");
+
+
+    printf("%s", category);
+
+    return category;
+}
+
+void generateRandomPair(float* x1, float* x2){
+    *x1 = ((float)rand() /((float)RAND_MAX /(UPPER - LOWER))) - 2;
+    *x2 = ((float)rand() /((float)RAND_MAX /(UPPER - LOWER))) - 2;
+}
+
