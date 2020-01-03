f49f776da874589cdc50c0940b29c01198544031
Nikos P <nikosp2196@gmail.com>
2020-01-03 18:11:32 +0200

    Finished the main logic of kmeans. Now I need to visualize the results.

--- kmeans.c
+++ kmeans.c
@@ -7,21 +7,31 @@
 
 #define FILENAME "groups.txt"
 #define DATA_SIZE 600
-#define M 3
-#define D 2
+#define M 3 // Number of clusters
+#define D 2 // Dimension of data
+#define R 5 // Number of executions
 
 
 void loadLine(int i);
 double getDistance(int i, int c);
 void initialize();
+void calculateAllDistances();
+void updateCentroids();
+int change();
+double getClusteringError();
 
 
 FILE* fp;
-int groups[DATA_SIZE];
-double data[DATA_SIZE][D];
-int clusters[M][DATA_SIZE]; //If the dataset was larger we should malloc.
-double centroids[M][D];
-int t;
+int groups[DATA_SIZE];              // True labels
+double data[DATA_SIZE][D];          // Data
+int clusters[R][DATA_SIZE];            // Predicted labels
+double previous_centroids[M][D];
+double centroids[R][M][D];
+int counter[M];                     // Counts the elements of every cluster
+double distances[R][DATA_SIZE][M];     // Stores all distances from every cluster
+int t;                              // Season counter
+int r;
+double errors[R];
 
 
 int main(int argc, char** argv) {
@@ -37,13 +47,29 @@ int main(int argc, char** argv) {
     for (int i = 0; i < DATA_SIZE; i++){
         loadLine(i);
     }
+    fclose(fp);
 
-    initialize();
+    for (r = 0; r < R; r++){
+        printf("----------EXECUTION: %d\n", r);
+        initialize();
+
+        while(change()){
+            printf("Season: %d\n", t);
+            calculateAllDistances();
+            updateCentroids();
+            t++;
+        }
+        errors[r] = getClusteringError();
+        printf("$$$$ ERROR = %lf $$$$\n", errors[r]);
+    }
+    
+    
+    /*
     printf("DATA--> x1 = %lf  x2 = %lf\n", data[0][0], data[0][1]);
     printf("CENTROID--> x1 = %lf  x2 = %lf\n", centroids[0][0], centroids[0][1]);
     printf("Distance: %f\n", getDistance(0,0));
+    */
     
-    fclose(fp);
 
 
     
@@ -52,23 +78,22 @@ int main(int argc, char** argv) {
 
 void loadLine(int i){
     char buff[10];
-    printf("%d\n",i);
+
     fscanf(fp, "%s", buff);
-    printf("%s\n", buff);
+
 
     groups[i] = (int)buff[1] - 48 - 1;
-    printf("%d\n", groups[i]);
+
 
     fscanf(fp, "%lf", &data[i][0]);
-    printf("%lf\n", data[i][0]);
+
 
     fscanf(fp, "%lf", &data[i][1]);
-    printf("%lf\n", data[i][1]);
 }
 
 double getDistance(int i, int c){
-    double diff_x1 = data[i][0] - centroids[c][0];
-    double diff_x2 = data[i][1] - centroids[c][1];
+    double diff_x1 = data[i][0] - centroids[r][c][0];
+    double diff_x2 = data[i][1] - centroids[r][c][1];
 
     double sqr1 = pow(diff_x1, 2);
     double sqr2 = pow(diff_x2, 2);
@@ -83,8 +108,71 @@ void initialize(){
     int temp;
     for(int i = 0; i < M; i++){
         temp = rand() % DATA_SIZE;
-        centroids[i][0] = data[temp][0];
-        centroids[i][1] = data[temp][1];
+        centroids[r][i][0] = data[temp][0];
+        centroids[r][i][1] = data[temp][1];
     }
     t = 0;
+}
+
+void calculateAllDistances(){
+    double mindist;
+    int cluster;
+    for (int i = 0; i < DATA_SIZE; i++){
+        mindist = 5;
+        cluster = -1;
+        for (int j = 0; j < M; j++){
+            distances[r][i][j] = getDistance(i,j);
+            if(mindist > distances[r][i][j]){
+                mindist = distances[r][i][j];
+                cluster = j;
+            }
+        }
+        clusters[r][i] = cluster;
+        counter[cluster]++;
+    }
+}
+
+void updateCentroids(){
+    double sums[M][D];
+    for (int i = 0; i < M; i++){
+        sums[i][0]=0;
+        sums[i][1]=0;
+        //printf("%d, %d, %d\n", counter[0],counter[1],counter[2]);
+    }
+    for (int i = 0; i < DATA_SIZE; i++){
+        sums[clusters[r][i]][0] += data[i][0];
+        sums[clusters[r][i]][1] += data[i][1];
+    }
+    //printf("%f, %f, %f\n", sums[0][0],sums[1][0],sums[2][0]);
+    //printf("%f, %f, %f\n", sums[0][1],sums[1][1],sums[2][1]);
+
+    for (int i = 0; i<M; i++){
+        //printf("OLD CENTROID--> x1 = %lf  x2 = %lf\n", centroids[r][i][0], centroids[r][i][1]);
+        previous_centroids[i][0] = centroids[r][i][0];
+        previous_centroids[i][1] = centroids[r][i][1];
+
+        centroids[r][i][0] = sums[i][0] / counter[i];
+        centroids[r][i][1] = sums[i][1] / counter[i];
+        //printf("NEW CENTROID--> x1 = %lf  x2 = %lf\n", centroids[r][i][0], centroids[r][i][1]);
+        counter[i] = 0;
+    }
+}
+
+int change(){
+    for(int i=0; i<M; i++){
+        if(centroids[r][i][0] != previous_centroids[i][0] || centroids[r][i][1] != previous_centroids[i][1]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+double getClusteringError(){
+    double error = 0;
+    for (int m = 0; m < M; m++){
+        for(int d = 0; d < DATA_SIZE; d++){
+            error += distances[r][d][clusters[r][d]];
+        }
+    }
+    return error;
 }
